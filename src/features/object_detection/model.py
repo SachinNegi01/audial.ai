1365cddc57f2536b03f88f3478e1fcbdbb784f2e
sachinnegi01 <snegi142003@gmail.com>
2026-02-05 13:47:54 +0530

    Added YOLO pretrained model in the projecr-- Currently detecting the images

--- src/features/object_detection/model.py
+++ src/features/object_detection/model.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
-from src.config import YOLO_MODEL_NAME
+from src.config import YOLO_MODEL_PATH
 import streamlit as st
 
 @st.cache_resource
 def load_yolo_model():
-    return YOLO(YOLO_MODEL_NAME)
+    return YOLO(YOLO_MODEL_PATH)
